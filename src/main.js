bb27470fe9cc8359e6da3a75c155135c1d1fcc83
kais <nagliyavarec@gmail.com>
2022-03-06 16:17:01 +0300

    added 'edit task' & 'task done'

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import './assets/css/style.css'
 import { createApp } from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faTrashArrowUp, faCirclePlus, faPenToSquare, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faTrashArrowUp, faCirclePlus, faPenToSquare, faCheck, faSquareCheck } from '@fortawesome/free-solid-svg-icons'
 
 import App from './App.vue'
 
-library.add(faTrashArrowUp, faCirclePlus, faPenToSquare, faCheck)
+library.add(faTrashArrowUp, faCirclePlus, faPenToSquare, faCheck, faSquareCheck)
 
 createApp(App)
   .component('fa', FontAwesomeIcon)
